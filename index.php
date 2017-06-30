3ceb68ad0fd58853a6b52cf3f44b2677e9bf741d
chantityb <chantityb@gmail.com>
2017-06-30 14:58:56 -0400

    added clearing functions

--- index.php
+++ index.php
@@ -1,11 +1,31 @@
 <?php
+    
+    // Start Session
+    session_start();
+    if(isset($_POST['name'])){
+        if(isset($_SESSION['bookmarks'])){
+          $_SESSION['bookmarks'][$_POST['name']] = $_POST['url'];  
+        }else {
+            $_SESSION['bookmarks'] = Array($_POST['name'] => $_POST['url']);
+        }
+    }
 
 
+    if(isset($_GET['action']) && $_GET['action'] == 'clear'){
+        session_unset();
+        session_destroy();
+        header('Location: index.php');
+        
+    }
 
-
-
+    if(isset($_GET['action']) && $_GET['action'] == 'delete'){
+        unset($_SESSION['bookmarks'][$_GET['name']]);
+        header('Location: index.php');
+        
+    }
 
 ?>
+
 
 
 
@@ -18,12 +38,20 @@
     
 <head>
    <title>Bookmarker</title>
-    <link rel="stylesheet" href="https://bootswatch.com/cyborg/bootstrap.min.css"    
+    <link rel="stylesheet" href="https://bootswatch.com/cyborg/bootstrap.min.css">
+    
+    <style>
+        .delete{color: white;}
+    
+    
+    
+    </style>
+          
 </head>
 
 
 <body>
-     <nav class="navbar navbar-inverse navbar-fixed-top">
+     <nav class="navbar navbar-inverse">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -38,7 +66,6 @@
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
             <li><a href="index.php">Home</a></li>
-            <li><a href="#about">About</a></li>
           </ul>
             
           <ul class="nav navbar-nav navbar-right">
@@ -48,11 +75,41 @@
       </div>
     </nav>
 
+    <div class="container">
     
+        <div class="row">
+        
+            <div class="col-md-7">
+                <form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+                    <div class="form-group">
+                        <label>Website Name</label>
+                        <input type="text" class="form-control" name="name"
+                    </div>
+                    <div class="form-group">
+                        <label>Website URL</label>
+                        <input type="text" class="form-control" name="url"
+                    </div>
+                    <input type="submit" value="submit" class="btn btn-default">
+                
+                </form>
+            
+            </div>
+            
+            <div class="col-md-5">
+                <?php if(isset($_SESSION['bookmarks'])) : ?>
+                    <ul class="list-group">
+                        <?php foreach($_SESSION['bookmarks'] as $name => $url) : ?>
+                        <li class="list-group-item"><a href="<?php echo $url; ?>"><?php echo $name; ?></a><a class="delete" href="index.php?action=delete&name=<?php echo $name; ?>">[X]</a></li>
+                            
+                        <?php endforeach; ?>
+                    </ul>
+                <?php endif; ?>
+            </div>
+        
+        </div>
+    
+    </div>
     
 </body>
-
-
-
 
 </html>

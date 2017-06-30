b342f35d876e6b3f4d2df50d5cca77c7cb63c49c
chantityb <chantityb@gmail.com>
2017-06-30 14:25:21 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+
+
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+
+
+<html>
+
+    
+    
+<head>
+   <title>Bookmarker</title>
+    <link rel="stylesheet" href="https://bootswatch.com/cyborg/bootstrap.min.css"    
+</head>
+
+
+<body>
+     <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Bookmarker</a>
+        </div>
+        
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="#about">About</a></li>
+          </ul>
+            
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="index.php?action=clear">Clear All</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    
+    
+</body>
+
+
+
+
+</html>
